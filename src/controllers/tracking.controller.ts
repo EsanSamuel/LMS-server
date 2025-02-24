9c50ef143d079cbf3a80835a2140440e911562e1
EsanSamuel <esansamuel555@gmail.com>
2025-02-25 00:36:04 +0100

    user can now track quizzes they've participated in

--- src/controllers/tracking.controller.ts
+++ src/controllers/tracking.controller.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import prisma from "../libs/prismadb";
 import { ApiError, ApiSuccess } from "../utils/ApiResponse";
-import { EngagedModules, EngagedCourses } from "@prisma/client";
+import { EngagedModules, EngagedCourses, EngagedRooms } from "@prisma/client";
 import Redis from "ioredis";
 
 const redis = new Redis();
@@ -62,7 +62,7 @@ class TrackingController {
         const cachedkey = `trackModules:${clerkId}`;
         //get cached user through key
         const cachedModules = await redis.get(cachedkey);
-        if (!cachedModules) {
+        if (cachedModules) {
           console.log(`Track modules ${clerkId} fetched from cache!`);
           res
             .status(200)
@@ -165,7 +165,7 @@ class TrackingController {
         const cachedkey = `trackCourses:${clerkId}`;
         //get cached user through key
         const cachedCourses = await redis.get(cachedkey);
-        if (!cachedCourses) {
+        if (cachedCourses) {
           console.log(`Track modules ${clerkId} fetched from cache!`);
           res
             .status(200)
@@ -203,6 +203,212 @@ class TrackingController {
                 200,
                 "Module added to user progress tarcker!",
                 getCourses
+              )
+            );
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json(new ApiError(500, "Something went wrong🔴🔴!", error));
+    }
+  }
+
+  static async trackRooms(req: express.Request, res: express.Response) {
+    try {
+      const { userId } = req.body;
+      const roomId = req.params.id;
+
+      const RoomViewedbyUser = await prisma.engagedRooms.create({
+        data: {
+          user: {
+            connect: {
+              clerkId: userId,
+            },
+          },
+          room: {
+            connect: {
+              id: roomId,
+            },
+          },
+        },
+      });
+      console.log(RoomViewedbyUser);
+      await redis.del(`trackRooms:${userId}`);
+      res
+        .status(201)
+        .json(
+          new ApiSuccess(
+            201,
+            "Rooms added to user progress tracker!",
+            RoomViewedbyUser
+          )
+        );
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json(new ApiError(500, "Something went wrong🔴🔴!", error));
+    }
+  }
+
+  static async getTrackRooms(req: express.Request, res: express.Response) {
+    try {
+      const clerkId = req.params.id;
+
+      const user = await prisma.user.findUnique({
+        where: {
+          clerkId: clerkId,
+        },
+      });
+      if (user) {
+        const userId = user.id;
+
+        const cachedkey = `trackRooms:${clerkId}`;
+        //get cached user through key
+        const cachedRooms = await redis.get(cachedkey);
+        if (cachedRooms) {
+          console.log(`Track modules ${clerkId} fetched from cache!`);
+          res
+            .status(200)
+            .json(
+              new ApiSuccess(
+                200,
+                "User gotten from cache successfully",
+                JSON.parse(cachedRooms)
+              )
+            );
+        } else {
+          const getRooms = await prisma.engagedRooms.findMany({
+            where: {
+              userId,
+            },
+            orderBy: {
+              addedAt: "desc",
+            },
+            include: {
+              user: true,
+              room: {
+                include: {
+                  Module: true,
+                },
+              },
+            },
+          });
+
+          console.log(getRooms);
+          await redis.setex(cachedkey, 600, JSON.stringify(getRooms));
+          res
+            .status(200)
+            .json(
+              new ApiSuccess(
+                200,
+                "Module added to user progress tarcker!",
+                getRooms
+              )
+            );
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json(new ApiError(500, "Something went wrong🔴🔴!", error));
+    }
+  }
+
+  static async trackUserQuiz(req: express.Request, res: express.Response) {
+    try {
+      const { userId } = req.body;
+      const quizId = req.params.id;
+
+      const addQuizToUserProgressTracker = await prisma.engagedQuiz.create({
+        data: {
+          user: {
+            connect: {
+              clerkId: userId,
+            },
+          },
+          quiz: {
+            connect: {
+              id: quizId,
+            },
+          },
+        },
+      });
+      console.log(addQuizToUserProgressTracker);
+      await redis.del(`trackQuiz:${userId}`);
+      res
+        .status(201)
+        .json(
+          new ApiSuccess(
+            201,
+            "Module added to user progress tracker!",
+            addQuizToUserProgressTracker
+          )
+        );
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json(new ApiError(500, "Something went wrong🔴🔴!", error));
+    }
+  }
+
+  static async getTrackQuiz(req: express.Request, res: express.Response) {
+    try {
+      const clerkId = req.params.id;
+
+      const user = await prisma.user.findUnique({
+        where: {
+          clerkId: clerkId,
+        },
+      });
+      if (user) {
+        const userId = user.id;
+
+        const cachedkey = `trackQuiz:${clerkId}`;
+        //get cached user through key
+        const cachedQuizzes = await redis.get(cachedkey);
+        if (cachedQuizzes) {
+          console.log(`Track modules ${clerkId} fetched from cache!`);
+          res
+            .status(200)
+            .json(
+              new ApiSuccess(
+                200,
+                "User gotten from cache successfully",
+                JSON.parse(cachedQuizzes)
+              )
+            );
+        } else {
+          const getQuiz = await prisma.engagedQuiz.findMany({
+            where: {
+              userId,
+            },
+            orderBy: {
+              addedAt: "desc",
+            },
+            include: {
+              user: true,
+              quiz: {
+                include: {
+                  questions: true,
+                },
+              },
+            },
+          });
+
+          console.log(getQuiz);
+          await redis.setex(cachedkey, 600, JSON.stringify(getQuiz));
+          res
+            .status(200)
+            .json(
+              new ApiSuccess(
+                200,
+                "Module added to user progress tarcker!",
+                getQuiz
               )
             );
         }
--- src/routes/v1/tracking.routes.ts
+++ src/routes/v1/tracking.routes.ts
@@ -7,5 +7,9 @@ router.post("/trackModule/:id", TrackingController.trackUserModules);
 router.get("/getTrackedModule/:id", TrackingController.getTrackModules);
 router.post("/trackCourse/:id", TrackingController.trackCourses);
 router.get("/getTrackedCourse/:id", TrackingController.getTrackCourses);
+router.post("/trackRoom/:id", TrackingController.trackRooms);
+router.get("/getTrackedRoom/:id", TrackingController.getTrackRooms);
+router.post("/trackQuiz/:id", TrackingController.trackUserQuiz);
+router.get("/getTrackedQuiz/:id", TrackingController.getTrackQuiz);
 
 export default router;
